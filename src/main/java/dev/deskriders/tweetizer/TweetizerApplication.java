6f036b918d9a92bff605909bdd440061003c11c0
namuan <575441+namuan@users.noreply.github.com>
2020-05-07 22:54:24 +0100

    Initial commit

---
+++ src/main/java/dev/deskriders/tweetizer/TweetizerApplication.java
@@ -0,0 +1,13 @@
+package dev.deskriders.tweetizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/deskriders/tweetizer/TweetizerApplicationShould.java
@@ -0,0 +1,13 @@
+package dev.deskriders.tweetizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetizerApplicationShould {
+
+	@Test
+	void loadApplicationContext() {
+	}
+
+}
